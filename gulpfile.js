84455ab215d33385480bfe8bcdf07e3739c0e96a
Denis <volad2114@gmail.com>
2020-09-09 00:06:36 +0700

    chore

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ let gulp = require('gulp'),
 	rename = require('gulp-rename'),
 	del = require('del'),
 	autoprefixer = require('gulp-autoprefixer'),
-	gs = require('gulp-selectors')
+	gs = require('gulp-selectors'),
+	cssmin = require('gulp-cssmin')
 const { dest } = require('gulp');
 
 
@@ -14,11 +15,6 @@ gulp.task('clean', async function () {
 	del.sync('dist')
 })
 
-gulp.task('concat', function () {
-	return gulp.src(['app/scss/**/*.scss', 'app/pages/**/*.scss'])
-		.pipe(concat('test.scss'))
-		.pipe(gulp.dest('app/css'))
-})
 
 gulp.task('scss', function () {
 	return gulp.src(['app/scss/**/*.scss'])
@@ -32,6 +28,18 @@ gulp.task('scss', function () {
 		.pipe(browserSync.reload({ stream: true }))
 });
 
+gulp.task('style', function () {
+	return gulp.src([
+		'node_modules/normalize.css/normalize.css',
+		'node_modules/slick-carousel/slick/slick.css',
+
+	])
+		.pipe(concat('lib.min.css'))
+		.pipe(cssmin())
+		.pipe(gulp.dest('app/css'))
+})
+
+// not used
 gulp.task('css', function () {
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
@@ -70,12 +78,15 @@ gulp.task('browser-sync', function () {
 	});
 });
 
-gulp.task('export', function () {
+gulp.task('export', async function () {
 	// let buildHtml = gulp.src('app/**/*.html')
 	// 	.pipe(gulp.dest('dist'));
 
 	// let BuildCss = gulp.src('app/css/**/*.css')
 	// 	.pipe(gulp.dest('dist/css'));
+
+	let Style = gulp.src('app/css/lib.min.css')
+		.pipe(gulp.dest('dist/css'))
 
 	let BuildJs = gulp.src('app/js/**/*.js')
 		.pipe(gulp.dest('dist/js'));
@@ -86,9 +97,9 @@ gulp.task('export', function () {
 	let BuildImg = gulp.src('app/img/**/*.*')
 		.pipe(gulp.dest('dist/img'));
 
-	let Selectors = gulp.src(['app/css/*.css', "app/index.html"], { base: 'app' })
+	let Selectors = gulp.src(['app/css/style.min.css', "app/index.html"], { base: 'app' })
 		.pipe(gs.run())
-		.pipe(gulp.dest(['dist']))
+		.pipe(gulp.dest('dist'))
 });
 
 gulp.task('watch', function () {
@@ -99,4 +110,4 @@ gulp.task('watch', function () {
 
 gulp.task('build', gulp.series('clean', 'export'))
 
-gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('style', 'scss', 'js', 'browser-sync', 'watch'));
